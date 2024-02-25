ca02aef0b0da0c595282a2e34b427d116b7edfce
kevinharv <kharvey2002@gmail.com>
2024-02-24 19:47:56 -0600

    DB scaffolding

--- db/createTables.sql
+++ db/createTables.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS saml_idps (
     idp_id          UUID            UNIQUE NOT NULL,
     domain          VARCHAR(63)     UNIQUE NOT NULL,
     metadata_url    VARCHAR(253)    NOT NULL,
-    is_enabled      BOOLEAN         DEFAULT(1),
-    created_at      DATETIME,
+    is_enabled      BOOLEAN         DEFAULT(TRUE),
+    created_at      TIMESTAMP,
     updated_at      TIMESTAMP,
 
     PRIMARY KEY (idp_id)
@@ -14,6 +14,8 @@ CREATE TABLE IF NOT EXISTS saml_idps (
 CREATE TABLE IF NOT EXISTS auth_methods (
     auth_id     UUID            UNIQUE NOT NULL,
     auth_name   VARCHAR(128)    UNIQUE NOT NULL,
+    created_at  TIMESTAMP,
+    updated_at  TIMESTAMP,
 
     PRIMARY KEY (auth_id)
 );
@@ -26,8 +28,10 @@ CREATE TABLE IF NOT EXISTS users (
     first_name          VARCHAR(128),
     last_name           VARCHAR(128),
     middle_init         CHAR(1),
-    display_name        VARCHAR(64)
+    display_name        VARCHAR(64),
+    created_at          TIMESTAMP,
+    updated_at          TIMESTAMP,
 
     PRIMARY KEY (user_id),
-    FOREIGN KEY (auth_method) REFERENCES auth_methods.auth_id
+    FOREIGN KEY (auth_method) REFERENCES auth_methods(auth_id)
 );
